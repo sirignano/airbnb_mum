213ef35878fc1a5270ada00bc4c266fe8cdb521b
Your Name <you@example.com>
2019-05-25 22:29:46 +0200

    first push

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+	def index
+	end
+
+	def calendar
+	end
+
+	def contact
+	end
+
+	def images
+	end
+
+	def guide
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+	root "home#index"
+	get "/" => "home#index"
+	get 'home/index'
+	get "/calendar" => 'home#calendar'
+	get "/guide" => 'home#guide'
+	get "/contact" => 'home#contact'
+	get "/images" => 'home#images'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
